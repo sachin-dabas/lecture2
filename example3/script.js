71d970f9dd9e7dcab3b5030853845705a8f41638
Sachin-dabas <sachin.dabas@outlook.com>
2021-01-23 05:21:09 +0530

    changing the background color

--- example3/script.js
+++ example3/script.js
@@ -16,7 +16,7 @@ function init() {
 
     // create a scene and a camera
     scene = new THREE.Scene()
-    scene.background = new THREE.Color(1,1,1)
+    scene.background = new THREE.Color(0,0,0)
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
     camera.position.y = - 100
 
